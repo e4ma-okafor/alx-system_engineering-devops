8976b720794ffa92ad06de0e5e41b90d3b72de6e
e4ma-okafor <ifeomasandra13@gmail.com>
2023-12-22 20:10:58 -0800

    puppet install

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Creates a file in /tmp
+# Create a file in /tmp
 
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/holberton',
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+  path    => '/tmp/school',
 }
