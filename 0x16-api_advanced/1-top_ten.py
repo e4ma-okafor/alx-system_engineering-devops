a83deed57ab3f12ae7100afe3ae35cf9a3c94450
e4ma-okafor <ifeomasandra13@gmail.com>
2024-02-15 02:13:28 +0000

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
-import requests
+"""
+defines function to query the Reddit API for
+prints titles of the first 10 hot posts
+"""
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+    """
+    queries the Reddit API
+    prints the titles of the first 10 hot posts for subreddit
+    """
+    import json
+    import requests
+    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
+        subreddit)
+    subreddit_info = requests.get(subreddit_URL,
+                                  headers={"user-agent": "user"},
+                                  allow_redirects=False).json()
+    if "data" not in subreddit_info:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    children = subreddit_info.get("data").get("children")
+    for child in children:
+        print(child.get("data").get("title"))
